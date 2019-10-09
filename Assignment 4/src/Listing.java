b6e6aa9456df66766e5db50e441bc103624f551d
Melanos <aikanaro@live.ru>
2019-10-08 23:56:59 -0400

    Lab 4 25-27

--- Assignment 4/src/Listing.java
+++ Assignment 4/src/Listing.java
@@ -1,37 +1,44 @@
 import java.util.Scanner;
 
-public class Listing {
+public class Listing
+{
     private String name;
     private String address;
     private String number;
 
-    Listing() {
+    Listing()
+    {
         name = "";
         address = "";
         number = "";
     }
 
-    private Listing(String n, String a, String _n) {
+    private Listing(String n, String a, String _n)
+    {
         name = n;
         address = a;
         number = _n;
     }
 
-    public String toString() {
+    public String toString()
+    {
         return (("The name is: " + name + ".") +
                 ("The address is: " + address + ("."))
                 + ("Number is: ") + number + ("."));
     }
 
-    Listing deepCopy() {
+    Listing deepCopy()
+    {
         return new Listing(name, address, number);
     }
 
-    int compareTo(String Name) {
+    int compareTo(String Name)
+    {
         return (name.compareTo(Name));
     }
 
-    void input() {
+    void input()
+    {
         Scanner job = new Scanner(System.in);
         System.out.println(" Enter a name ");
         name = job.nextLine();
@@ -40,9 +47,9 @@ public class Listing {
         System.out.println(" Enter student street address: ");
         address = job.nextLine();
     }
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         StudentList student = new StudentList();
-
     }
 }
 
--- Assignment 4/src/SinglyLinkedList.java
+++ Assignment 4/src/SinglyLinkedList.java
@@ -1,4 +1,5 @@
-class SinglyLinkedList {
+class SinglyLinkedList
+{
 
     private Node h;
 
